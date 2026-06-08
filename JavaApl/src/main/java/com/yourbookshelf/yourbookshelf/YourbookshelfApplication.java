538a5b6948aedf956e15cb99cd76c5c7be9257a6
KotoPorot <tim1709976@gmail.com>
2026-06-08 11:12:53 +0200

    Initial commit

---
+++ JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/YourbookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.yourbookshelf.yourbookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourbookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourbookshelfApplication.class, args);
+	}
+
+}
