31d52dda48e032e9d1f95b298089ef3547b4b104
Tim <tim1709976@gmail.com>
2026-07-17 07:44:46 +0200

    feat: update get api and add delete point

--- JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/controller/MyNoteController.java
+++ JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/controller/MyNoteController.java
@@ -5,7 +5,7 @@ import com.yourbookshelf.yourbookshelf.DTO.MyNoteResponseDTO;
 import com.yourbookshelf.yourbookshelf.DTO.MyUserPrincipal;
 import com.yourbookshelf.yourbookshelf.service.entity_service.MyNoteService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,24 +14,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/notes")
 public class MyNoteController {
     private final MyNoteService noteService;
 
-
-    @PostMapping("/add")
-    public ResponseEntity<MyNoteResponseDTO> addNote(@AuthenticationPrincipal MyUserPrincipal principal,
-                                                      @RequestBody @Valid MyNoteRequestDTO request){
+    //"/api/v1/notes/create"
+    @PostMapping("/create")
+    public ResponseEntity<MyNoteResponseDTO> createNote(@AuthenticationPrincipal MyUserPrincipal principal,
+                                                        @RequestBody @Valid MyNoteRequestDTO request){
 
         return ResponseEntity.status(HttpStatus.CREATED).body(noteService.createNote(request, principal.getUser()));
     }
 
-    @GetMapping
-    public ResponseEntity<List<MyNoteResponseDTO>> getNotesByBookId(@AuthenticationPrincipal MyUserPrincipal principal,
-                                                                     @RequestParam(name = "bookId") Long bookId){
-        return ResponseEntity.ok(noteService.getByBookId(bookId, principal.getUser()));
+    //"/api/v1/notes/get-user-notes" ==== all user notes
+    //"/api/v1/notes/get-user-notes?bookId=1" ==== notes for the book with id 1
+    @GetMapping("/get-user-notes")
+    public ResponseEntity<List<MyNoteResponseDTO>> getNotesByUserId(@AuthenticationPrincipal MyUserPrincipal principal,
+                                                                    @RequestParam(name = "bookId", required = false) Long bookId){
+
+        if (bookId!=null){
+            return ResponseEntity.ok(noteService.getByBookId(bookId, principal.getUser()));
+        }
+
+        return ResponseEntity.ok(noteService.getByUser(principal.getUser()));
     }
 
+    //"/api/v1/notes/delete/1"  ==== delete note with id 1
+    @DeleteMapping("/delete/{noteId}")
+    public ResponseEntity<Void> deleteNote(@AuthenticationPrincipal MyUserPrincipal principal,
+                                           @PathVariable Long noteId){
+        noteService.deleteNote(noteId, principal.getUser());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }
