a6be35ad8f525917f757e3d9c522d1a05a187d37
Tim <tim1709976@gmail.com>
2026-08-11 08:08:50 +0200

    feat: add liquibase migration for myFolder

--- JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/entity/flashcard/MyFolder.java
+++ JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/entity/flashcard/MyFolder.java
@@ -19,7 +19,7 @@ public class MyFolder {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 500)
     private String name;
 
 
