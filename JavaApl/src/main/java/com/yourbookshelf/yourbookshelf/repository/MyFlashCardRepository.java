9a1b8235c0f98aa018b2ee77b5f883429e551e42
Tim <tim1709976@gmail.com>
2026-08-16 08:01:41 +0200

    fix: rename parameters in MyFlashCardRepository

--- JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/repository/MyFlashCardRepository.java
+++ JavaApl/src/main/java/com/yourbookshelf/yourbookshelf/repository/MyFlashCardRepository.java
@@ -11,8 +11,8 @@ import java.util.List;
 public interface MyFlashCardRepository extends JpaRepository<MyFlashCard, Long> {
 
     @Query("SELECT DISTINCT c FROM MyFlashCard c LEFT JOIN FETCH c.examples WHERE c.user.id = :userId")
-    List<MyFlashCard> findAllByUserIdWithExamples(Long id);
+    List<MyFlashCard> findAllByUserIdWithExamples(Long userId);
 
     @Query("SELECT DISTINCT c FROM MyFlashCard c LEFT JOIN FETCH c.examples WHERE c.user.id = :userId AND c.folder.id = :folderId")
-    List<MyFlashCard> findAllByUserIdAndFolderIdWithExamples(Long id, Long folderId);
+    List<MyFlashCard> findAllByUserIdAndFolderIdWithExamples(Long userId, Long folderId);
 }
